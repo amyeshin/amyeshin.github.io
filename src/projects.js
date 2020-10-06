411ed71319b081bc1fbc1bc722e83f9ccbd5555b
amyeshin <amy.e.shin@gmail.com>
2020-10-06 12:53:56 -0400

    update project descriptions

--- src/projects.js
+++ src/projects.js
@@ -8,11 +8,49 @@ function Projects(){
         <div>
             <div className="project-container">
                 <h3>C4ME</h3>
-
+                <div className="info-container">
+                    <div className="bullet-wrapper">
+                        <ul>
+                            <li>Spearheaded the frontend for a single-page
+                            application to help students manage their college
+                            application process</li>
+                            <li>Designed wireframes with Figma and built those
+                            pages in the frontend with backend integration
+                            and routing</li>
+                            <li>Rendered an interactive scatter plot that
+                            represents test score data of students who applied
+                            to a selected college and their admission
+                            decisions</li>
+                            <li>Implemented pagination to showcase college and
+                            high school card components</li>
+                        </ul>
+                    </div>
+                    <div className="project-img-wrapper">
+                        <img className="tabguard-size" src={TabGuard} alt="" />
+                    </div>
+                </div>
             </div>
+
             <div className="project-container">
                 <h3>Rating Predictor</h3>
-
+                <div className="info-container">
+                    <div className="bullet-wrapper">
+                        <ul>
+                            <li>Implemented a sentiment analysis system with word
+                            to vector embeddings to predict ratings given product
+                            reviews</li>
+                            <li>Optimized the rating predictor with user-factor
+                            adaptation and computed predictions with Principal
+                            Component Analysis</li>
+                            <li>Applied deep learning principles by creating a
+                            Long Short-Term Memory Network to further improve
+                            predictions by 2%</li>
+                        </ul>
+                    </div>
+                    <div className="project-img-wrapper">
+                        <img className="tabguard-size" src={TabGuard} alt="" />
+                    </div>
+                </div>
             </div>
             <div className="project-container">
                 <h3>TabGuard</h3>
