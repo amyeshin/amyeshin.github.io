e3e33d4668ba3f0883385fd00365e14014fad32e
amyeshin <amy.e.shin@gmail.com>
2020-10-06 16:22:41 -0400

    Updates

---
+++ precache-manifest.37e22afb507df8ac640a22555eca9816.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed8f23ec0ed34e2350027a7b0bfe1a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f33b2a9910f376f7f4",
+    "url": "/static/css/main.c24f63a5.chunk.css"
+  },
+  {
+    "revision": "ad07192e877cdba59f69",
+    "url": "/static/js/2.e1fef502.chunk.js"
+  },
+  {
+    "revision": "348e18c767030a34e3e00df7559622cb",
+    "url": "/static/js/2.e1fef502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f33b2a9910f376f7f4",
+    "url": "/static/js/main.ec5e8168.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  },
+  {
+    "revision": "eef925ee7c96d137ce97b64bff1d30e9",
+    "url": "/static/media/TabGuard.eef925ee.png"
+  },
+  {
+    "revision": "0c74ddfbeab8ab10428db13a7454f842",
+    "url": "/static/media/c4me.0c74ddfb.png"
+  },
+  {
+    "revision": "2b6f567b112523973e0ead978ebd7b2c",
+    "url": "/static/media/personal_photo.2b6f567b.png"
+  },
+  {
+    "revision": "c75dd6361f6ae504675d9efa578440a1",
+    "url": "/static/media/rating_predictor.c75dd636.gif"
+  }
+]);
